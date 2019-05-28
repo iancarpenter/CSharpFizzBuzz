de9965b7aeb9c9e70a3db735ff5ba436c5f55b28
Ian Carpenter <ianhcarpenter@gmail.com>
2019-05-28 19:49:12 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSharpFizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+
+                if (fizz == true && buzz == true)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (fizz == true)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (buzz == true)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            Console.Read();
+        }
+    }
+}
